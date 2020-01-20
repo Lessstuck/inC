39602ae1e2ac6a25c6ab0b00e8b119d6bb7ef945
Les Stuck <les@lesstuck.com>
2020-01-20 11:40:32 -0700

    completed record_preferences

--- tideman.c
+++ tideman.c
@@ -16,7 +16,8 @@ typedef struct
 {
     int winner;
     int loser;
-}x2pair;
+}
+pair;
 
 // Array of candidates
 string candidates[MAX];
@@ -41,7 +42,7 @@ int main(int argc, string argv[])
         printf("Usage: tideman [candidate ...]\n");
         return 1;
     }
-
+    
     // Populate array of candidates
     candidate_count = argc - 1;
     if (candidate_count > MAX)
@@ -53,7 +54,7 @@ int main(int argc, string argv[])
     {
         candidates[i] = argv[i + 1];
     }
-
+    
     // Clear graph of locked in pairs
     for (int i = 0; i < candidate_count; i++)
     {
@@ -62,10 +63,10 @@ int main(int argc, string argv[])
             locked[i][j] = false;
         }
     }
-
+    
     pair_count = 0;
     int voter_count = get_int("Number of voters: ");
-
+    
     // Clear preferences - I had to add this despite the specifications
     for (int j = 0; j < MAX; j++)
     {
@@ -74,39 +75,39 @@ int main(int argc, string argv[])
             preferences[j][k] = 0;
         }
     }
-
+    
     // Query for votes
     for (int i = 0; i < voter_count; i++)
     {
         // ranks[i] is voter's ith preference
         int ranks[candidate_count];
-
+        
         // Query for each rank
         for (int j = 0; j < candidate_count; j++)
         {
             string name = get_string("Rank %i: ", j + 1);
-
+            
             if (!vote(j, name, ranks))
             {
                 printf("Invalid vote.\n");
                 return 3;
             }
         }
-
+        
         record_preferences(ranks);
         // delete this debuggery
         for (int j = 0; j < candidate_count; j++)
         {
             for (int k = 0; k < candidate_count; k++)
             {
-                printf("%d ", preferences[j][k]);
+                printf("%d ", preferences[k][j]);
             }
             printf("\n");
         }
-
+        
         printf("\n");
     }
-
+    
     add_pairs();
     sort_pairs();
     lock_pairs();
@@ -131,14 +132,17 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
+    int remaining_candidates = candidate_count;
     for (int i = 0; i < candidate_count - 1; i++)
     {
-        for (int j = i + 1; j < candidate_count - 1 - i; j++)
+        int r = ranks[i];
+        for (int j = i + 1; j < candidate_count; j++)
         {
-            preferences[ranks[i]][ranks[j]]++;
+            preferences[r][ranks[j]]++;
         }
+        remaining_candidates--;
     }
-
+    
     return;
 }
 
