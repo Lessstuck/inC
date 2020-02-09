3b084bed44e091a26aea5c871e9ad62ca2b8da34
Les Stuck <les@lesstuck.com>
2020-02-08 17:50:31 -0700

    gray passed tests! framework for reflect done

--- helpers.c
+++ helpers.c
@@ -16,19 +16,40 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             blue = image[i][j].rgbtBlue;
             green = image[i][j].rgbtGreen;
             red = image[i][j].rgbtRed;
-            gray = ceil(((float)(blue + green + red) / 3.));
+            gray = round(((float)(blue + green + red) / 3.0));
             image[i][j].rgbtBlue = gray;
             image[i][j].rgbtGreen = gray;
             image[i][j].rgbtRed = gray;
         }
     }
-    // printf("%i %i %i %f\n", blue, green, red, gray);
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE transformedImage[height][width];
+    if (width % 2 == 0)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                transformedImage[i][j] = image[i][width - j - 1];
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                transformedImage[i][j] = image[i][width - j - 1];
+            }
+        }
+    }
+    image = transformedImage;
     return;
 }
 
