c0c0c9195c321e166e7e0e15d7fd5a21c126381a
Les Stuck <les@lesstuck.com>
2020-02-15 13:41:28 -0700

    reads jpeg headers

--- recover/recover.c
+++ recover/recover.c
@@ -6,7 +6,7 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    // open files for reading and writing
+    // open file for reading
     if (argc != 2)
     {
         printf("Please include one and only one argument–the raw file to be recovered\n");
@@ -18,39 +18,44 @@ int main(int argc, char *argv[])
     {
         return 1;
     }
-    FILE *writePtr = fopen("out.raw", "w");
-    if (rawPtr == NULL)
-    {
-        return 1;
-    }
+
     // get 512-byte buffers
-    char gotc;
+    BYTE gotc;
     BYTE buffer[512];
-    int j = 0;
-    int k = 0;
+    long j = 0;
+    long k = 0;
     int jpegCounter = 0;
-    for (long i = 0; i < 3744768; i = i + 512) //size from Finder
+    char filename[8];
+    for (long i = 0; i < 7314; i++)  // magic number of blocks in test file
     {
         for (j = 0; j < 512; j++)
         {
-            while((gotc = fgetc(rawPtr)) != EOF)
+            gotc = fgetc(rawPtr);
             {
                 buffer[j] = gotc;
             }
         }
+        // open first file for writing
+        sprintf("filename", "%03i.jpg", jpegCounter);
+        FILE *writePtr = fopen(filename, "w");
+        if (rawPtr == NULL)
+        {
+            return 1;
+        }
 
         // look for jpeg header
-        // if (buffer[0] == 0xff)
-        // && buffer[1] == 0xd8 && buffer[2] == 0xff)
-        // &&
-        if ((buffer[3] & 0xf0) == 0xe0)
+        if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
         {
             jpegCounter++;
+            printf("jpegCounter: %i, block: %li\n", jpegCounter, i);
+            for (k = 0; k < 512; k++)
+            {
+                fputc(buffer[k], writePtr);
+            }
         }
+            fclose(writePtr);
     }
-    // fputc(buffer[], writePtr);
+
     fclose(rawPtr);
-    fclose(writePtr);
-    printf("jpegCounter is %i\n", jpegCounter);
     return 0;
 }
