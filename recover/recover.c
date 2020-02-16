6d737810dccdd7160f612f37716dd6c5751dc1db
Les Stuck <les@lesstuck.com>
2020-02-16 13:48:53 -0700

    100% correct

--- recover/recover.c
+++ recover/recover.c
@@ -13,13 +13,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *rawPtr = fopen( argv[1], "r");
+    FILE *rawPtr = fopen(argv[1], "r");
     if (rawPtr == NULL)
     {
         return 1;
     }
 
-    // get 512-byte buffers
     BYTE gotc;
     BYTE buffer[512];
     int bufferSize = 0;
@@ -27,44 +26,46 @@ int main(int argc, char *argv[])
     long j = 0;
     int k = 0;
     int jpegCounter = 0;
+    int isNew = 0;
     char writeFile[50];
     FILE *writePtr = NULL;
 
-    do
+    while ((bufferSize = fread(buffer, 1, 512, rawPtr)) != 0)
     {
-        bufferSize = fread(buffer, 1, 512, rawPtr);
-
-        // {
-        //     fclose(rawPtr);
-        //     fclose(writePtr);
-        //     return 0;
-        // }
+        // bufferSize = fread(buffer, 1, 512, rawPtr);
         // look for jpeg header
         if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
         {
+            isNew = 1;
             // close previous file unless this is the first one
-            // if (jpegCounter != 0)
-            // {
-            //     // fclose(writePtr);
-            // }
+            if (jpegCounter != 0)
+            {
+                fclose(writePtr);
+            }
 
             // name and write new file
             sprintf(writeFile, "%03i.jpg", jpegCounter);
-            writePtr = fopen(writeFile, "w");
+            writePtr = fopen(writeFile, "a");
             if (writePtr == NULL)
             {
                 return 1;
             }
             jpegCounter++;
-            printf("jpegCounter: %i\n", jpegCounter);
-        // segmentation fault happens HERE               <----------------------
-        fwrite(buffer, 1, 512, writePtr);
-        fclose(writePtr);
-
+            fwrite(buffer, 1, 512, writePtr);
+        }
+        else if (isNew == 0)
+        {
+            continue; //necessary to deal with first non-image bytes
+        }
+        else if (isNew == 1)
+        {
+            fwrite(buffer, 1, 512, writePtr); // appending
         }
 
     }
-    while (bufferSize == 512);
+
+    // clean it up and go home!
+    fclose(writePtr);
     fclose(rawPtr);
     return 0;
 }
